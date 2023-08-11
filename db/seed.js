53dac12e0c8971e94f700f6c9a72744f0c98e404
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-08-11 15:05:25 -0600

    added products and pets_products data

--- db/seed.js
+++ db/seed.js
@@ -1,21 +1,21 @@
-const client = require('./db.js');
+const client = require("./db.js");
 
-const dropTables = async() => {
+const dropTables = async () => {
   try {
     await client.query(`
     DROP TABLE IF EXISTS pets_products;
     DROP TABLE IF EXISTS products;
     DROP TABLE IF EXISTS pets;  
-      DROP TABLE IF EXISTS owners;
-    `);
+    DROP TABLE IF EXISTS owners;
+  `);
 
-    console.log('TABLES DROPPED!');
-  } catch(error) {
+    console.log("TABLES DROPPED!");
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
-const createTables = async() => {
+const createTables = async () => {
   try {
     await client.query(`
       CREATE TABLE owners(
@@ -41,56 +41,93 @@ const createTables = async() => {
       );
     `);
 
-    console.log('TABLES CREATED!');
-  } catch(error) {
+    console.log("TABLES CREATED!");
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
-const createOwner = async(ownersName) => {
+const createOwner = async (ownersName) => {
   try {
     await client.query(`
       INSERT INTO owners (Name)
       VALUES ('${ownersName}');
     `);
 
-    console.log('OWNER CREATED');
-  } catch(error) {
-    console.log(error)
+    console.log("OWNER CREATED");
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
-const createPet = async(petName, petType, petOwnerId) => {
+const createPet = async (petName, petType, petOwnerId) => {
   try {
     await client.query(`
       INSERT INTO pets (name, type, owner_id)
       VALUES ('${petName}', '${petType}', ${petOwnerId});
     `);
-    console.log('PET CREATED')
+    console.log("PET CREATED");
   } catch (err) {
     console.log(err);
   }
+};
+
+const createProduct = async (productName) => {
+  try {
+    await client.query(`
+      INSERT INTO products (name)
+      VALUES ('${productName}');
+    `);
+    console.log("PRODUCT CREATED");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-const syncAndSeed = async() => {
+const createPetsProducts = async (petId, productId) => {
+  try {
+    await client.query(`
+      INSERT INTO pets_products (pets_id, products_id)
+      VALUES (${petId}, ${productId});
+    `);
+    console.log('PETS_PRODUCTS CREATED');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const syncAndSeed = async () => {
   try {
     await client.connect();
-    console.log('CONNECTED TO THE DB!');
+    console.log("CONNECTED TO THE DB!");
 
     await dropTables();
     await createTables();
 
-    await createOwner('Greg');
+    await createOwner("Greg");
     await createOwner(null);
-    await createOwner('Bill');
+    await createOwner("Bill");
 
-    await createPet('Floffy', 'Bunny', 1);
-    await createPet('Leroy', 'Lizard', 1);
-    await createPet('Fido', 'Doggo', 3);
+    await createPet("Floffy", "Bunny", 1);
+    await createPet("Leroy", "Lizard", 1);
+    await createPet("Fido", "Doggo", 3);
 
-  } catch(error) {
+    await createProduct("Food");
+    await createProduct("Terrerium");
+    await createProduct("Cage");
+    await createProduct("Leash");
+
+    await createPetsProducts(1, 1);
+    await createPetsProducts(1, 3);
+    await createPetsProducts(2, 1);
+    await createPetsProducts(2, 2);
+    await createPetsProducts(3, 1);
+    await createPetsProducts(3, 3);
+    await createPetsProducts(3, 4);
+
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
 syncAndSeed();
